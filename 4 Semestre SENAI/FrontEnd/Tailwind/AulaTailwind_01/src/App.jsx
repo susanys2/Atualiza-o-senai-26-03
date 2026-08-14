70588214a5ccde80f6f5d88d2689cab3df1c261b
Susany <suzenilinda@gmail.com>
2026-08-14 08:31:56 -0300

    Aula SENAI

---
+++ 4 Semestre SENAI/FrontEnd/Tailwind/AulaTailwind_01/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="bg-black flex">
+      <div className="relative-group" >
+      <button className="text-black font-bold bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90% rounded-3xl p-5 m-5 w-40 transition">Clique Aqui</button>
+       </div>
+      <button className="text-black font-bold bg-radial-[at_50%_75%] from-pink-200 via-pink-400 to-pink-900 to-90% rounded-3xl p-5 m-5 w-40" >Clique Aqui</button>
+      <button className="text-black font-bold bg-radial-[at_50%_75%] from-fuchsia-200 via-fuchsia-400 to-fuchsia-900 to-90% rounded-3xl p-5 m-5 w-40" >Clique Aqui</button>
+
+      
+    </div>
+
+    
+  )
+}
