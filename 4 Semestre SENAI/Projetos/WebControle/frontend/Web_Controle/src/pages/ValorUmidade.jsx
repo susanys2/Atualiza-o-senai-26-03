1e3dec6287b9033209ea577752f8c24251759e26
Susany <suzenilinda@gmail.com>
2026-08-21 15:48:49 -0300

    Aulas SENAI

--- 4 Semestre SENAI/Projetos/WebControle/frontend/Web_Controle/src/pages/ValorUmidade.jsx
+++ 4 Semestre SENAI/Projetos/WebControle/frontend/Web_Controle/src/pages/ValorUmidade.jsx
@@ -28,8 +28,9 @@ export default function TelaNivelUmidade(){
 
     return (
         <div className='flex flex-col items-center justify-center'>
-    <h1 className="text-4xl font-bold text-black font-['Limelight']">Tela de Controle do Nivel de Umidade</h1>
-    <p>Umidade: {nivelUmidade}</p>
+    <h1 className="text-4xl font-bold text-black font-['Limelight']">Tela de Controle do Nivel de Umidade do Solo</h1>
+    <p>Umidade Status: {nivelUmidade}</p>
+    <a href='https://wokwi.com/projects/472984055914357761'>Monitor de Umidade</a>
     </div>
     )
 }
